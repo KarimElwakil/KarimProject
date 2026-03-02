3248b48a3e1ddb8fa9b04fee01beae29600f1da1
KarimElwakil <karim.gamer2020@gmail.com>
2026-03-02 09:09:12 +0200

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -86,7 +86,7 @@ settingsRef.once("value").then(snap=>{
     settingsRef.set({
       notify:true,
       banned:false
-    }).then(()=>{
+   .then(()=>{
       startAnalytics();
     });
   }else{
@@ -1071,9 +1071,10 @@ function sendFinalSummary(){
 }
 
 summaryTimer=setTimeout(sendFinalSummary,1200000); //20 min
-});
+
 
 }); // نهاية DOMContentLoaded
+
 
 
 
